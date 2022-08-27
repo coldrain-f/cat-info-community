acda4a05693a5a826610f570e19ad468d0e6326e
coldrain-f <intmain__@naver.com>
2022-08-27 17:12:21 +0900

    Add: GlobalPipes에 ValidationPipe 등록
    class-validator를 사용하기 위해서 main.ts에서 ValidationPipe 등록

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const SERVER_PORT = process.env.SERVER_PORT;
+
+  // class-validator를 사용하려면 전역 파이프에 ValidationPipe()를 등록해 줘야 한다.
+  app.useGlobalPipes(new ValidationPipe());
+
   await app.listen(SERVER_PORT);
 }
 bootstrap();
