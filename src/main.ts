819caa4a7def77f5d7b9eaa1902b5699abfebb11
coldrain-f <intmain__@naver.com>
2022-08-28 00:01:17 +0900

    Add: API 문서 제공을 위한 Swagger 설정 추가

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -8,6 +9,17 @@ async function bootstrap() {
 
   // class-validator를 사용하려면 전역 파이프에 ValidationPipe()를 등록해 줘야 한다.
   app.useGlobalPipes(new ValidationPipe());
+
+  // 스웨거 설정 추가
+  const config = new DocumentBuilder()
+    .setTitle('C.I.C')
+    .setDescription('cat')
+    .setVersion('1.0.0')
+    .addTag('')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
 
   await app.listen(SERVER_PORT);
 }
